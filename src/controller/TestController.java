8de8c76e5b90368c73cdde7cac0489b81e292622
ucceoum <ucceoum@gmail.com>
2020-03-11 12:35:46 +0900

    controller 수정

--- src/controller/TestController.java
+++ src/controller/TestController.java
@@ -12,7 +12,12 @@ public class TestController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		request.setCharacterEncoding("utf-8");
+		
+		String requestURI = request.getRequestURI();
+		String contextPath = request.getContextPath();
+		String cmd = requestURI.substring(contextPath.length());
+		
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
